61901b5acc641a350c6b837061a87791b975b5ea
Larry Jones <mrwizard82d1@gmail.com>
2025-10-30 18:23:23 -0500

    Invoking function passing args
    Remember that `args`  must either be
    
    - The string representation of a Clojure map
    - A set of key-value pairs

--- src/dev/core.clj
+++ src/dev/core.clj
@@ -6,3 +6,18 @@
 ;; `clojure.main` is to invoke (`core.deps`).
 (defn -main []
   (println "Main function called"))
+
+
+;; This function can be invoked using the `-X` option like
+;; `clj -X dev.core/print-args :key1 "value1" :key2 "value2"`. Specifying
+;; the arguments like this, using key-value pairs, generates a map
+;; containing these key value pairs.
+;;
+;; An alternative is to specify the map as a single string argument
+;; containing a Clojure string representation of a map; that is,
+;; `clj -X dev.core/printargs "{:key1 value1, :key2 value2}"`.
+;;
+;; The result of both these command line formats is the same.
+(defn print-args [args]
+  "IMPORTANT: the argument, `args`, must be a **map**."
+  (println "Passed arguments: " args))
