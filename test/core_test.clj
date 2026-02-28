ffa1bfcbe40753fa91e47987d7dee1ad4cbc5aaf
Larry Jones <mrwizard82d1@gmail.com>
2026-02-28 17:54:59 -0600

    Trying to execute code in `test` directory fails
    `clj` reports an error like:
    
    > Execution error (FileNotFoundException) at....
    > Could not locate...on classpath....
    
    This error results from our "empty" `deps.edn` configuration.

---
+++ test/core_test.clj
@@ -0,0 +1,12 @@
+(ns core-test
+  (:require  [clojure.test :as t]))
+
+;; We will add a very simple `-main` function to our `core-test`
+;; namespace and try to run this function.
+;;
+;; When I try to execute this function using `clj`, `clj` reports
+;; an error: "Execution error (FileNotFoundException)....Could
+;; not locate... on the classpath...."
+
+(defn -main []
+  (println "Testing in progress."))
